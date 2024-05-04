51b521465d565914582af26c65ef2e6c28bb929e
TIMER_err <zqq2333@outlook.com>
2024-05-04 13:07:07 +0800

    updated

--- Core/src/cn/yapeteam/yolbi/module/Module.java
+++ Core/src/cn/yapeteam/yolbi/module/Module.java
@@ -24,7 +24,7 @@ public abstract class Module {
     private ModuleCategory category = null;
     private int key = 0;
 
-    private boolean enabled = false;
+    protected boolean enabled = false;
 
     private boolean listening = false;
 
