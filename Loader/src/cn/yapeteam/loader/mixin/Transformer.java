e923110901dc547689294c4f848609d7d9e30788
TIMER_err <zqq2333@outlook.com>
2024-05-04 12:51:00 +0800

    Revert "Revert "Added Self Destruct but will crash the client""
    This reverts commit d11f0429e1b4f4936d29b33a567b36b858230c9d.

--- Loader/src/cn/yapeteam/loader/mixin/Transformer.java
+++ Loader/src/cn/yapeteam/loader/mixin/Transformer.java
@@ -6,8 +6,11 @@ import cn.yapeteam.loader.mixin.operation.impl.OverwriteOperation;
 import cn.yapeteam.loader.utils.ASMUtils;
 import cn.yapeteam.loader.utils.ClassUtils;
 import lombok.Getter;
+import org.apache.commons.io.IOUtils;
 import org.objectweb.asm_9_2.tree.ClassNode;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +20,9 @@ public class Transformer {
     private final ClassProvider provider;
     private final ArrayList<Mixin> mixins;
     private final ArrayList<Operation> operations;
+
+    @Getter
+    private byte[] oldBytes;
 
     public Transformer(ClassProvider classProvider) {
         this.provider = classProvider;
@@ -32,15 +38,38 @@ public class Transformer {
         mixins.add(new Mixin(source, theClass, provider));
     }
 
-    public Map<String, byte[]> transform() {
+    public Map<String, byte[]> transform() throws IOException {
         Map<String, byte[]> classMap = new HashMap<>();
         for (Mixin mixin : mixins) {
             for (Operation operation : operations)
                 operation.dispose(mixin);
             String name = mixin.getTarget().name.replace('/', '.');
+            oldBytes = getClassBytes(mixin.getTarget().getClass());
             byte[] class_bytes = ASMUtils.rewriteClass(mixin.getTarget());
             classMap.put(name, class_bytes);
         }
         return classMap;
     }
+
+    public Map<String, byte[]> transformoldbytes() throws IOException {
+        Map<String, byte[]> classMap = new HashMap<>();
+        for (Mixin mixin : mixins) {
+            for (Operation operation : operations)
+                operation.dispose(mixin);
+            String name = mixin.getTarget().name.replace('/', '.');
+            oldBytes = getClassBytes(mixin.getTarget().getClass());
+            byte[] class_bytes = ASMUtils.rewriteClass(mixin.getTarget());
+            classMap.put(name, oldBytes);
+        }
+        return classMap;
+    }
+
+
+    public static byte[] getClassBytes(Class<?> c) throws IOException {
+        String className = c.getName();
+        String classAsPath = className.replace('.', '/') + ".class";
+        InputStream stream = c.getClassLoader().getResourceAsStream(classAsPath);
+        return stream == null ? null : IOUtils.toByteArray(stream);
+    }
+
 }
