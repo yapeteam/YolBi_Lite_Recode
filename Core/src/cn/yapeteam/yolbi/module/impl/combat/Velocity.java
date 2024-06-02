bc56522273518315ea50f5d17d45531749a37a56
neilhuang007 <neilhuang007@gmail.com>
2024-06-02 10:23:41 +0800

    Velocity jump reset written

--- Core/src/cn/yapeteam/yolbi/module/impl/combat/Velocity.java
+++ Core/src/cn/yapeteam/yolbi/module/impl/combat/Velocity.java
@@ -2,18 +2,26 @@ package cn.yapeteam.yolbi.module.impl.combat;
 
 import cn.yapeteam.loader.api.module.ModuleCategory;
 import cn.yapeteam.loader.api.module.ModuleInfo;
+import cn.yapeteam.loader.api.module.values.impl.BooleanValue;
 import cn.yapeteam.loader.api.module.values.impl.NumberValue;
 import cn.yapeteam.yolbi.YolBi;
 import cn.yapeteam.yolbi.event.Listener;
+import cn.yapeteam.yolbi.event.impl.game.EventTick;
 import cn.yapeteam.yolbi.event.impl.network.EventPacket;
 import cn.yapeteam.yolbi.event.impl.player.EventUpdate;
 import cn.yapeteam.yolbi.module.Module;
 import cn.yapeteam.yolbi.notification.Notification;
 import cn.yapeteam.yolbi.notification.NotificationType;
 import cn.yapeteam.yolbi.utils.animation.Easing;
+import cn.yapeteam.yolbi.utils.math.MathUtils;
 import cn.yapeteam.yolbi.utils.reflect.ReflectUtil;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.network.play.server.S12PacketEntityVelocity;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @ModuleInfo(name = "Velocity", category = ModuleCategory.COMBAT)
 public class Velocity extends Module {
@@ -21,8 +29,25 @@ public class Velocity extends Module {
 
     private final NumberValue<Double> vertical = new NumberValue<>("Verical velocity", 80.0, 0.0, 100.0, 1.0);
 
+    private final BooleanValue jump = new BooleanValue("Jump", true);
+
+    //probability for having a perfect jump
+    private final NumberValue<Double> probability = new NumberValue<>("Probability", 100.0, 0.0, 100.0, 1.0);
+
+    //delay for jumping after the perfect jump
+    private final NumberValue<Double> maxJumpDelay = new NumberValue<>("Max Jump Delay", 0.0, 0.0, 1000.0, 10.0);
+
+    private final NumberValue<Double> minJumpDelay = new NumberValue<>("Min Jump Delay", 0.0, 0.0, 1000.0, 10.0);
+
+    // how long you hold the space bar
+    private final NumberValue<Double> maxJumpHold = new NumberValue<>("Max Jump Hold", 0.0, 0.0, 1000.0, 10.0);
+
+    private final NumberValue<Double> minJumpHold = new NumberValue<>("Min Jump Hold", 0.0, 0.0, 1000.0, 10.0);
+
+
+
     public Velocity() {
-        addValues(horizontal, vertical);
+        addValues(horizontal, vertical, jump, probability, maxJumpDelay, minJumpDelay, maxJumpHold, minJumpHold);
     }
 
     @Listener
@@ -35,6 +60,22 @@ public class Velocity extends Module {
         } else if (mc.thePlayer.hurtTime > 1) {
             ReflectUtil.SetPressed(mc.gameSettings.keyBindJump, GameSettings.isKeyDown(mc.gameSettings.keyBindJump));
         }
+    }
+
+    public void jumpreset(){
+        ReflectUtil.SetPressed(mc.gameSettings.keyBindJump, true);
+
+        // Create a ScheduledExecutorService
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        // Generate a random delay in milliseconds
+        int delay = (int) MathUtils.getRandom(minJumpHold.getValue().doubleValue(), maxJumpDelay.getValue().doubleValue()); // replace 1000 with the maximum delay you want
+
+        // Schedule a task to set the jump keybind to false after the delay
+        executorService.schedule(() -> ReflectUtil.SetPressed(mc.gameSettings.keyBindJump, false), delay, TimeUnit.MILLISECONDS);
+
+        // Shut down the executor service
+        executorService.shutdown();
     }
 
     @Listener
@@ -60,9 +101,21 @@ public class Velocity extends Module {
             double newZ = z / 8000.0D * horizontal.getValue();
 
             // Set the new velocities
-            ReflectUtil.setMotionX(mc.thePlayer, newX);
-            ReflectUtil.setMotionY(mc.thePlayer, newY);
-            ReflectUtil.setMotionZ(mc.thePlayer, newZ);
+            ReflectUtil.setMotionX(mc.thePlayer, 0);
+            ReflectUtil.setMotionY(mc.thePlayer, 0);
+            ReflectUtil.setMotionZ(mc.thePlayer, 0);
+
+            if(jump.getValue()){
+                if(Math.random() * 100 < probability.getValue()){
+                    jumpreset();
+                }else{
+                    // this means to delay the jump
+                    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+                    int delay = (int) MathUtils.getRandom(minJumpDelay.getValue().doubleValue(), maxJumpDelay.getValue().doubleValue());
+                    executorService.schedule(() -> jumpreset(), delay, TimeUnit.MILLISECONDS);
+                    executorService.shutdown();
+                }
+            }
         }
     }
 }
