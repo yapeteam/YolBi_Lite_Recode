0e7d17dda48aa1298b0d7190d4642a31779eff4d
wzhy233 <133610954+wzhy233@users.noreply.github.com>
2024-06-30 10:28:53 +0800

    fix sprint

--- Core/src/cn/yapeteam/yolbi/module/impl/movement/Sprint.java
+++ Core/src/cn/yapeteam/yolbi/module/impl/movement/Sprint.java
@@ -1,31 +1,35 @@
 package cn.yapeteam.yolbi.module.impl.movement;
 
-import cn.yapeteam.loader.Natives;
+import cn.yapeteam.loader.api.module.ModuleCategory;
+import cn.yapeteam.loader.api.module.ModuleInfo;
+import cn.yapeteam.loader.api.module.values.impl.BooleanValue;
 import cn.yapeteam.yolbi.event.Listener;
-import cn.yapeteam.yolbi.event.impl.player.EventStrafe;
+import cn.yapeteam.yolbi.event.impl.player.EventMotion;
 import cn.yapeteam.yolbi.module.Module;
-import cn.yapeteam.yolbi.module.ModuleCategory;
-import cn.yapeteam.yolbi.module.ModuleInfo;
-import cn.yapeteam.yolbi.utils.misc.VirtualKeyBoard;
-import cn.yapeteam.yolbi.utils.player.PlayerUtil;
-import cn.yapeteam.yolbi.utils.player.RotationManager;
-import cn.yapeteam.yolbi.utils.player.RotationsUtil;
-import cn.yapeteam.yolbi.utils.vector.Vector2f;
-
 
 @ModuleInfo(name = "Sprint", category = ModuleCategory.MOVEMENT)
 public class Sprint extends Module {
+    private boolean sprinting;
+    private final BooleanValue autoSprint = new BooleanValue("AutoSprint", true);
+
+    {
+        addValues(autoSprint);
+    }
 
     @Listener
-    private void onStrafe(EventStrafe event) {
-        if (mc.thePlayer.isSneaking() || RotationManager.targetRotations != null && RotationsUtil.getRotationDifference(new Vector2f(mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch)) > 30F) {
-            return;
-        }
-
-        if (PlayerUtil.isMoving() && !mc.thePlayer.isSprinting()) {
-            Natives.SetKeyBoard(VirtualKeyBoard.VK_W, false);
-            Natives.SetKeyBoard(VirtualKeyBoard.VK_W, true);
-//            mc.thePlayer.setSprinting(true);
+    private void onMotion(EventMotion event) {
+        if (autoSprint.getValue()) {
+            if (mc.thePlayer != null && mc.thePlayer.movementInput.moveForward > 0) {
+                if (!mc.thePlayer.isSprinting()) {
+                    mc.thePlayer.setSprinting(true);
+                    sprinting = true;
+                }
+            } else {
+                if (sprinting) {
+                    mc.thePlayer.setSprinting(false);
+                    sprinting = false;
+                }
+            }
         }
     }
 }
