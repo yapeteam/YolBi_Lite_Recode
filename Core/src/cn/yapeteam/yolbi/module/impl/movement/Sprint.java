6128ade1f8dd1858d059894e7dae7a437d8f64e6
ZeathDev <zeathdev@outlook.com>
2024-06-30 14:09:08 +0800

    没用的设置

--- Core/src/cn/yapeteam/yolbi/module/impl/movement/Sprint.java
+++ Core/src/cn/yapeteam/yolbi/module/impl/movement/Sprint.java
@@ -5,32 +5,24 @@ import cn.yapeteam.yolbi.event.impl.player.EventMotion;
 import cn.yapeteam.yolbi.module.Module;
 import cn.yapeteam.yolbi.module.ModuleCategory;
 import cn.yapeteam.yolbi.module.ModuleInfo;
-import cn.yapeteam.yolbi.module.values.impl.BooleanValue;
 
 @ModuleInfo(name = "Sprint", category = ModuleCategory.MOVEMENT)
 public class Sprint extends Module {
     private boolean sprinting;
-    private final BooleanValue autoSprint = new BooleanValue("AutoSprint", true);
-
-    public Sprint() {
-        addValues(autoSprint);
-    }
 
     @Listener
     private void onMotion(EventMotion event) {
         if (mc.thePlayer == null) return;
 
-        if (autoSprint.getValue()) {
-            if (mc.thePlayer.movementInput.moveForward > 0) {
-                if (!mc.thePlayer.isSprinting()) {
-                    mc.thePlayer.setSprinting(true);
-                    sprinting = true;
-                }
-            } else {
-                if (sprinting) {
-                    mc.thePlayer.setSprinting(false);
-                    sprinting = false;
-                }
+        if (mc.thePlayer.movementInput.moveForward > 0) {
+            if (!mc.thePlayer.isSprinting()) {
+                mc.thePlayer.setSprinting(true);
+                sprinting = true;
+            }
+        } else {
+            if (sprinting) {
+                mc.thePlayer.setSprinting(false);
+                sprinting = false;
             }
         }
     }
