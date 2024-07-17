864cf66b4e0825c204dbcbe1ad1b13cb3613636f
StarsWhite <2939353631@qq.com>
2024-07-17 18:38:57 +0800

    new

--- Core-1.12.2/src/cn/yapeteam/yolbi/module/ModuleManager.java
+++ Core-1.12.2/src/cn/yapeteam/yolbi/module/ModuleManager.java
@@ -10,6 +10,7 @@ import cn.yapeteam.yolbi.module.impl.movement.KeepSprint;
 import cn.yapeteam.yolbi.module.impl.movement.StrafeFix;
 import cn.yapeteam.yolbi.module.impl.movement.Sprint;
 import cn.yapeteam.yolbi.module.impl.player.AutoArmor;
+import cn.yapeteam.yolbi.module.impl.player.ChestStealer;
 import cn.yapeteam.yolbi.module.impl.player.InvCleaner;
 import cn.yapeteam.yolbi.module.impl.visual.*;
 import cn.yapeteam.yolbi.notification.Notification;
