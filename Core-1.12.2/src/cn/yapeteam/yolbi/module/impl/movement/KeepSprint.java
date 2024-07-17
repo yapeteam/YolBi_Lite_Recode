d91ef35353a26d876e459e3676cbf15424f531f7
StarsWhite <2939353631@qq.com>
2024-07-17 20:40:46 +0800

    操你妈keepsprint

--- Core-1.12.2/src/cn/yapeteam/yolbi/module/impl/movement/KeepSprint.java
+++ Core-1.12.2/src/cn/yapeteam/yolbi/module/impl/movement/KeepSprint.java
@@ -1,10 +1,12 @@
 package cn.yapeteam.yolbi.module.impl.movement;
 
+import cn.yapeteam.yolbi.event.impl.network.EventPacket;
 import cn.yapeteam.yolbi.module.ModuleCategory;
 import cn.yapeteam.yolbi.module.values.impl.BooleanValue;
 import cn.yapeteam.yolbi.event.Listener;
 import cn.yapeteam.yolbi.event.impl.player.EventMotion;
 import cn.yapeteam.yolbi.module.Module;
+import net.minecraft.network.play.client.CPacketEntityAction;
 
 public class KeepSprint extends Module {
     private final BooleanValue keepSprint = new BooleanValue("KeepSprint", true);
@@ -15,11 +17,16 @@ public class KeepSprint extends Module {
     }
 
     @Listener
-    private void onMotion(EventMotion event) {
-        if (keepSprint.getValue()) {
-            if (mc.player != null && mc.player.isSprinting()) {
-                mc.player.setSprinting(true);
+    private void onMotion(EventPacket event) {
+        EventPacket e = event;
+        try {
+            if (e.isIncoming() && e.getPacket() instanceof CPacketEntityAction) {
+                CPacketEntityAction packet = (CPacketEntityAction) e.getPacket();
+                if (packet.getAction() == CPacketEntityAction.Action.STOP_SPRINTING) {
+                    e.setCancelled(true);
+                }
             }
+        } catch (ClassCastException exception) {
         }
     }
 }
