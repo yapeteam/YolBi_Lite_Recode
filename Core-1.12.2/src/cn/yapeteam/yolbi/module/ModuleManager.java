73c039a87e28e259f98cc8b0507602f5c71b42fc
TIMER_err <zqq2333@outlook.com>
2024-07-17 18:23:39 +0800

    你媽的背包管理大師

--- Core-1.12.2/src/cn/yapeteam/yolbi/module/ModuleManager.java
+++ Core-1.12.2/src/cn/yapeteam/yolbi/module/ModuleManager.java
@@ -9,6 +9,8 @@ import cn.yapeteam.yolbi.module.impl.movement.Eagle;
 import cn.yapeteam.yolbi.module.impl.movement.KeepSprint;
 import cn.yapeteam.yolbi.module.impl.movement.StrafeFix;
 import cn.yapeteam.yolbi.module.impl.movement.Sprint;
+import cn.yapeteam.yolbi.module.impl.player.AutoArmor;
+import cn.yapeteam.yolbi.module.impl.player.InvCleaner;
 import cn.yapeteam.yolbi.module.impl.visual.*;
 import cn.yapeteam.yolbi.notification.Notification;
 import cn.yapeteam.yolbi.notification.NotificationType;
@@ -56,6 +58,8 @@ public class ModuleManager {
         modules.add(new PacketDebug());
         modules.add(new Rotations());
         modules.add(new TargetHud());
+        modules.add(new AutoArmor());
+        modules.add(new InvCleaner());
 
         modules.sort((m1, m2) -> -Integer.compare(m2.getName().charAt(0), m1.getName().charAt(0)));
     }
