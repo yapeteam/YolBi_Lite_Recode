d4b1221a5187a5bc6f2f4e0b15caf45d165a50ce
wzhy233 <133610954+wzhy233@users.noreply.github.com>
2024-07-18 16:16:50 +0800

    1

--- Core-1.18.1/src/cn/yapeteam/yolbi/YolBi.java
+++ Core-1.18.1/src/cn/yapeteam/yolbi/YolBi.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class YolBi {
     public static YolBi instance = new YolBi();
     public static final String name = "YolBi Lite";
-    public static final String version = "0.3.5";
+    public static final String version = "0.3.6";
     public static final File YOLBI_DIR = new File(System.getProperty("user.home"), ".yolbi");
     public static boolean initialized = false;
     private EventManager eventManager;
