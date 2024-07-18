5daf22b14cd0ac1e0e21363e2eb1c5fff4770da3
wzhy233 <133610954+wzhy233@users.noreply.github.com>
2024-07-18 16:31:44 +0800

    Add files via upload

---
+++ Core-1.18.1/src/cn/yapeteam/yolbi/module/impl/movement/Sprint.java
@@ -0,0 +1,35 @@
+package cn.yapeteam.yolbi.module.impl.movement;
+
+import cn.yapeteam.loader.api.module.ModuleCategory;
+import cn.yapeteam.loader.api.module.ModuleInfo;
+import cn.yapeteam.loader.api.module.values.impl.BooleanValue;
+import cn.yapeteam.yolbi.event.Listener;
+import cn.yapeteam.yolbi.event.impl.player.EventMotion;
+import cn.yapeteam.yolbi.module.Module;
+
+@ModuleInfo(name = "Sprint", category = ModuleCategory.MOVEMENT)
+public class Sprint extends Module {
+    private boolean sprinting;
+    private final BooleanValue autoSprint = new BooleanValue("Sprint", true);
+
+    {
+        addValues(autoSprint);
+    }
+
+    @Listener
+    private void onMotion(EventMotion event) {
+        if (autoSprint.getValue()) {
+            if (mc.thePlayer != null && mc.thePlayer.movementInput.moveForward > 0) {
+                if (!mc.thePlayer.isSprinting()) {
+                    mc.thePlayer.setSprinting(true);
+                    sprinting = true;
+                }
+            } else {
+                if (sprinting) {
+                    mc.thePlayer.setSprinting(false);
+                    sprinting = false;
+                }
+            }
+        }
+    }
+}
