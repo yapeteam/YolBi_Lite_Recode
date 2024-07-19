46c443021550fa7206eac40ceafe7174077cad07
wzhy233 <wzhy233@outlook.com>
2024-07-19 12:02:41 +0800

    fixed

--- Core-1.12.2/src/cn/yapeteam/yolbi/server/WebServer.java
+++ Core-1.12.2/src/cn/yapeteam/yolbi/server/WebServer.java
@@ -1,46 +1,6 @@
-//package cn.yapeteam.yolbi.server;
-//
-//
-//import cn.yapeteam.loader.logger.Logger;
-//import cn.yapeteam.yolbi.server.handlers.modules.*;
-//import com.sun.net.httpserver.HttpServer;
-//import java.io.IOException;
-//import java.net.InetSocketAddress;
-//import java.util.concurrent.Executors;
-//
-//public class WebServer {
-//
-//    private static HttpServer server;
-//    public static final int PORT = 1145;
-//
-//    public static void start() throws IOException {
-//
-//        server = HttpServer.create(new InetSocketAddress("localhost", PORT), 0);
-//        server.createContext("/", new HtmlHttpHandler());
-//        server.createContext("/api/modulesList", new ModulesHttpHandler());
-////        server.createContext("/api/setStatus", new StatusHttpHandler());
-//        server.createContext("/api/updateModulesInfo", new ModuleInfoHttpHandler());
-////        server.createContext("/api/categoriesList", new CategoriesHttpHandler());
-//        server.createContext("/api/getModuleSetting", new ModuleSettingsHttpHandler());
-//        server.createContext("/api/setModuleSettingValue", new SetModuleSettingsHttpHandler());
-//        //server.createContext("/api/getAltAccounts", new HtmlAltAccountHandler());
-//        //server.createContext("/api/AltLogin", new HtmlAltLoginHandler());
-//        //server.createContext("/api/DeleteAlt", new HtmlAltDeleteHandler());
-//        //server.createContext("/api/AddAlt", new HtmlAddAltHandler());
-////        server.createContext("/api/setBind" , new BindHttpHandler());
-//        server.setExecutor(Executors.newFixedThreadPool(10));
-//
-//        server.start();
-//        Logger.info("Server started on port {}", PORT);
-//    }
-//
-//    public static void stop() {
-//        server.stop(0);
-//    }
-//}
 /**
  * @author wzhy233
- * @Time 2024-07-19  11:53
+ * @time 2024-07-19  11:53
  */
 
 package cn.yapeteam.yolbi.server;
@@ -58,7 +18,7 @@ public class WebServer {
 
     private static HttpServer server;
     private static final int DEFAULT_PORT = 23333;
-    private static final int MAX_PORT = 95535;
+    private static final int MAX_PORT = 99999;
 
     public static void start() throws IOException {
         int port = findAvailablePort(DEFAULT_PORT);
@@ -91,7 +51,10 @@ public class WebServer {
         Random random = new Random();
 
         while (!isPortAvailable(port)) {
+
             port = random.nextInt((MAX_PORT - DEFAULT_PORT) + 1) + DEFAULT_PORT;
+            Logger.info("Port not available!");
+            Logger.info("Found available port {}", port);
         }
 
         return port;
