db6b071669b781a133bab3b4363884b56733f0a0
wzhy233 <wzhy233@outlook.com>
2024-07-24 19:53:38 +0800

    Yolbi盾与服务器握手

--- Injector/src/cn/yapeteam/injector/Main.java
+++ Injector/src/cn/yapeteam/injector/Main.java
@@ -1,67 +1,3 @@
-//package cn.yapeteam.injector;
-//
-//import com.formdev.flatlaf.intellijthemes.FlatXcodeDarkIJTheme;
-//
-//import javax.swing.*;
-//        import java.io.File;
-//import java.io.IOException;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//
-//public class Main {
-//    public static final String version = "0.3.6";
-//    public static final File YolBi_Dir = new File(System.getProperty("user.home"), ".yolbi");
-//    public static final String dllName = "libinjection.dll";
-//    public static final int port = 20181;
-//
-//    public static void main(String[] args) throws Exception {
-//        Utils.unzip(Main.class.getResourceAsStream("/injection.zip"), YolBi_Dir);
-//        System.load(new File(Main.YolBi_Dir, "libapi.dll").getAbsolutePath());
-//        UIManager.setLookAndFeel(new FlatXcodeDarkIJTheme());
-//
-//        StartFrame startFrame = new StartFrame();
-//        startFrame.setVisible(true);
-//
-//        if (checkConnection()) {
-//            startFrame.dispose();
-//            MainFrame frame = new MainFrame();
-//            new Thread(() -> frame.setVisible(true)).start();
-//            if (args.length == 1) {
-//                frame.inject_dll(Integer.parseInt(args[0]));
-//                frame.inject_ui();
-//            }
-//        } else {
-//            startFrame.dispose();
-//            JOptionPane.showMessageDialog(null, "Failed to connect to the Yolbi Server", "Connection Error", JOptionPane.ERROR_MESSAGE);
-//            System.exit(0);
-//        }
-//    }
-//
-//    private static boolean checkConnection() {
-//        try {
-//
-//            URL url = new URL("http://yapeteam.github.io");
-//            URL url2 = new URL("http://skidonion.tech");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            HttpURLConnection connection2 = (HttpURLConnection) url2.openConnection();
-//            connection.setRequestMethod("HEAD");
-//            connection.setConnectTimeout(5000);
-//            connection.setReadTimeout(5000);
-//
-//            connection2.setRequestMethod("HEAD");
-//            connection2.setConnectTimeout(5000);
-//            connection2.setReadTimeout(5000);
-//            int responseCode = connection.getResponseCode();
-//            int responseCode2 = connection2.getResponseCode();
-//            return (200 <= responseCode && responseCode <= 399 && responseCode2 >= 200 && responseCode2 <= 399);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
-//    }
-//}
-//
-
 package cn.yapeteam.injector;
 import com.formdev.flatlaf.intellijthemes.FlatXcodeDarkIJTheme;
 import java.awt.Toolkit;
@@ -79,6 +15,7 @@ import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static final String version = "0.3.6";
